9fd68785ddc4b9f66756b40c96c7a672c214eac7
MutabPato <patrickmtbz@gmail.com>
2023-11-30 09:29:59 -0800

    dynamic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-i = 1
-len = len(sys.argv)
-print("{} {}".format(
-    len - 1,
-    "arguments." if len == 1 else "argument:" if len == 2 else "arguments:"
-    ))
-for argument in sys.argv[1:]:
-    print("{}: {}".format(i, argument))
-    i += 1
+if __name__ == "__main__":
+    import sys
+    i = 1
+    len = len(sys.argv)
+    print("{} {}".format(
+        len - 1,
+        "arguments." if len == 1 else "argument:" if len == 2 else "arguments:"
+        ))
+    for argument in sys.argv[1:]:
+        print("{}: {}".format(i, argument))
+        i += 1
