1b8021191a0504142c76995de006378a4252b51c
MutabPato <patrickmtbz@gmail.com>
2023-12-02 11:02:01 -0800

    Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for sub in matrix:
+        for index, elem in enumerate(sub):
+            print("{}".format(elem), end="")
+            if index < len(sub) - 1:
+                print(" ",end="")
+        print()
