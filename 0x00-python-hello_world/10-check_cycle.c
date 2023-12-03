acde368400b9ed3288ba8cd47709cff9b853610c
MutabPato <patrickmtbz@gmail.com>
2023-12-03 08:13:43 -0800

    Cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,7 +9,7 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *slow = list;
-	listint_t *fast = list->next;
+	listint_t *fast = (list != NULL) ? list->next : NULL;
 
 	if (list == NULL || list->next == NULL)
 		return (0);
