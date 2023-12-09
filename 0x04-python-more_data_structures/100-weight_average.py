b11f9e14e4e3f62cc7200afef52834b21378b87a
MutabPato <patrickmtbz@gmail.com>
2023-12-09 08:18:25 -0800

    Weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return (0) 
+    total_sum = 0
+    total_weight = 0
+
+    for item in my_list:
+        weight, value = item
+        total_sum += weight * value
+        total_weight += weight
+
+    if total_weight == 0:
+        return (0)
+
+    return (total_sum / total_weight)
