3200281e262274f5eb9aafaeabb05044b520ae1c
MutabPato <patrickmtbz@gmail.com>
2023-12-19 11:51:47 -0800

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    """Empty class that defines a square
-    No more class information
-    No more class information
-
-    Attributes:
-        no attributes in this class
-
+    """Defines a square
+    No further information
     """
     pass
