0dd2194066b36dcc7bc256a2d74f419384808c5e
MutabPato <patrickmtbz@gmail.com>
2023-12-19 12:46:43 -0800

    size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""No module description"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+        """__init__ method
+        Arguments:
+            size: size attribute
+        """
+        self.__size = size
