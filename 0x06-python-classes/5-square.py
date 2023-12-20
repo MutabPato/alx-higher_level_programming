3b93532e27314ee64a43394cabeb2c812ed082cf
MutabPato <patrickmtbz@gmail.com>
2023-12-20 07:51:13 -0800

    print a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -43,12 +43,10 @@ class Square:
 
     def my_print(self):
         """prints a square"""
-        i = 0
-        j = 0
         if self.__size == 0:
             print()
         else:
             for i in range(self.__size):
                 for j in range(self.__size):
                     print("#", end="")
-                print("\n")
+                print()
