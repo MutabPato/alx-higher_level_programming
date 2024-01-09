b56961e92b163dbcb2b4807298f558d553ab514f
MutabPato <patrickmtbz@gmail.com>
2024-01-09 09:51:11 -0800

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+
+No class created
+
+"""
+
+
+def read_file(filename=""):
+    """
+
+    read_file: reads a text file (UTF8) and prints it to stdout
+
+    """
+
+    with open(filename, encoding="utf-8") as f:
+        read_file = f.read()
+
+    print(read_file)
