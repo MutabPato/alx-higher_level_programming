5d260aff0da38b9a0dd0662af38711b449de4c0d
MutabPato <patrickmtbz@gmail.com>
2024-01-10 10:30:02 -0800

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+
+No class created
+
+"""
+
+
+class Student():
+    """
+
+    returns the dictionary description with simple data structure
+
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return ({
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age
+            })
