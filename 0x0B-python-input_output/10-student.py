f888388e952fc749ba3e03a11737e892132e1807
MutabPato <patrickmtbz@gmail.com>
2024-01-10 11:20:24 -0800

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -22,11 +22,10 @@ class Student():
         self.attrs = attrs
 
         if attrs is not None:
-            for attr in attrs:
-                if hasattr(self, attr):
-                    return ({
-                        attr: getattr(self, attr)
-                        })
+            return ({
+                attr: getattr(
+                    self, attr) for attr in attrs if hasattr(self, attr)
+                })
 
         return ({
             "first_name": self.first_name,
