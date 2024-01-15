d1e977c136228cc2323e900a077fe4ca0a12b6cc
MutabPato <patrickmtbz@gmail.com>
2024-01-15 13:59:36 -0800

    JSON

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,6 +15,9 @@ class Base():
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Initialises Base
+        """
 
         if id is not None:
             self.id = id
@@ -25,10 +28,28 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """
+        Returns: JSON string representation of list_dictionaries
+        """
+
         if list_dictionaries is None:
             return ("[]")
         return (json.dumps(list_dictionaries))
-    """
+
     def save_to_file(cls, list_objs):
-        with open(
-    """
+        """
+        writes the JSON string representation of list_objs to a file
+        """
+
+        if list_objs is None:
+            list_objs = []
+
+        class_name = cls.__name__
+
+        filename = f"{class_name}.json"
+
+        list_dicts = [obj.to_dictionary() for obj in list_objs]
+        json_string = cls.to_json_string(list_dicts)
+
+        with open(filename, 'w') as file:
+            file.write(json_string)
