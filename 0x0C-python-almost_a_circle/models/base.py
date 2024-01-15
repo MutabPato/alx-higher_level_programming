9c855d7987ffa99a24e43affb1ccb469899e07d9
MutabPato <patrickmtbz@gmail.com>
2024-01-15 14:18:12 -0800

    JSON

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,6 +36,7 @@ class Base():
             return ("[]")
         return (json.dumps(list_dictionaries))
 
+    @classmethod
     def save_to_file(cls, list_objs):
         """
         writes the JSON string representation of list_objs to a file
