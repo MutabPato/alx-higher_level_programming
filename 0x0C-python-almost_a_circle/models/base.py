01df0962d2ed6933d56347686faddd18b1eff704
MutabPato <patrickmtbz@gmail.com>
2024-01-16 09:14:19 -0800

    File to instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -78,3 +78,24 @@ class Base():
         dummy_instance.update(**dictionary)
 
         return (dummy_instance)
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Returns a list of instances
+        """
+
+        class_name = cls.__name__
+
+        filename = f"{class_name}.json"
+
+        try:
+            with open(filename, 'r') as file:
+                json_string = file.read()
+        except FileNotFoundError:
+            return ([])
+
+        list_dicts = cls.from_json_string(json_string)
+        instances = [cls.create(**data) for data in list_dicts]
+
+        return (instances)
