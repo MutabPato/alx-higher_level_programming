a28fde0ff7990947bfb7c06393691baae154232f
MutabPato <patrickmtbz@gmail.com>
2024-03-14 12:40:15 -0700

    print rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,6 +8,18 @@ class Rectangle {
     this.width = w;
     this.height = h;
   }
+
+  print () {
+    let i = 0; let j = 0; let row = '';
+
+    for (i = 0; i < this.width; i++) {
+      row += 'X';
+    }
+
+    for (j = 0; j < this.height; j++) {
+      console.log(row);
+    }
+  }
 }
 
 module.exports = Rectangle;
