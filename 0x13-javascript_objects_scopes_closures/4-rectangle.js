f0eee32017cf7a015feb5311278afcb872169c00
MutabPato <patrickmtbz@gmail.com>
2024-03-17 08:43:59 -0700

    Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -20,6 +20,15 @@ class Rectangle {
       console.log(row);
     }
   }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
