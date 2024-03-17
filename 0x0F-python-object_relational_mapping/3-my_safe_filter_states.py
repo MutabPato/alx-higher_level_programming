78ebcc80f46e5d2fed0e2234b15420ef5dd50eba
MutabPato <patrickmtbz@gmail.com>
2024-03-17 02:13:41 -0700

    SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" lists all states from the database hbtn0e0usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    mydb = MySQLdb.connect(
+            host="localhost", user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = mydb.cursor()
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %S", (match, ))
+    results = cur.fetchall()
+    for row in results:
+        print(row)
+    cur.close()
+    mydb.close()
