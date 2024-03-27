7c5a967aff0dd4eb2e687912ea2f1b64ead70e32
MutabPato <patrickmtbz@gmail.com>
2024-03-27 14:08:00 -0700

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #cURL POST parameters
-curl -Ls -X POST -d "email"="test@gmail.com"&"subject"="I will always be here for PLD" "$1"
+curl -Ls  "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
