72b9685870ee1ad55e74714461348c3466afac7f
MutabPato <patrickmtbz@gmail.com>
2024-03-27 11:07:52 -0700

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #cURL body size
-curl -s "$1" -o response.txt -w "%{http_code}" | awk '/^200$/{getline; print}'
+curl -s -o response.txt -w "%{http_code}" "$1" | tail -n 1 | grep -q "200" && cat response.txt | sed '$d'
