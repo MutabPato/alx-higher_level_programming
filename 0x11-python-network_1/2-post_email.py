453b4a3f51cdb7425ed0d4e214a427af10811142
MutabPato <patrickmtbz@gmail.com>
2024-03-28 13:41:47 -0700

    POST an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,14 +7,16 @@ import urllib.parse
 import urllib.request
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
-values = {"email": email}
 
-data = urllib.parse.urlencode(values)
-data = data.encode('utf-8')
-req = urllib.request.Request(url, data)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {"email": email}
 
-with urllib.request.urlopen(req) as response:
-    body = response.read()
-print("Your email is:", body.decode('utf-8'))
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+    print("Your email is:", body.decode('utf-8'))
