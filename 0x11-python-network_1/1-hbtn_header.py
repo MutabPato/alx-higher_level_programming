0f22efcfbe1f4090915c62f9a61a8afe444e48ad
MutabPato <patrickmtbz@gmail.com>
2024-03-28 13:39:26 -0700

    request

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,8 @@ import urllib.request
 import sys
 
 
-if __name__ == "__main__"
-with urllib.request.urlopen(sys.argv[1]) as response:
-    html = response.info()
-    request = html.get('X-Request-Id')
-print(request)
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        request = html.get('X-Request-Id')
+    print(request)
