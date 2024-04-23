e05adb339d301a55cc630682acc27f60eaf0eec1
MutabPato <patrickmtbz@gmail.com>
2024-04-23 10:44:07 -0700

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length < 2) {
+  console.error('Usage: 0-readme.js <URL>');
+  process.exit(1);
+}
+
+const URL = process.argv[1];
+
+request.get(URL, (err, res) => {
+  if (err) {
+	  console.error('Error: ', err);
+	  return;
+  }
+  console.log('Code: ', res.statusCode);
+});
