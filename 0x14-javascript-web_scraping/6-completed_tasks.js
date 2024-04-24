54abcad056f5867238a21538d966c1a793f86252
MutabPato <patrickmtbz@gmail.com>
2024-04-24 08:58:02 -0700

    Todos

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiURL = process.argv[2];
+
+request.get(apiURL, (err, res, body) => {
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+
+  const tasksCompletedByUser = {};
+
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (tasksCompletedByUser[todo.userId]) {
+        tasksCompletedByUser[todo.userId]++;
+      } else {
+        tasksCompletedByUser[todo.userId] = 1;
+      }
+    }
+  });
+  Object.entries(tasksCompletedByUser).forEach(([userId, tasksCompleted]) => { console.log(`${userId}: ${tasksCompleted}`); });
+});
